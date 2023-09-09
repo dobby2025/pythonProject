1ce653dc3f2a9ff86dffd5cc56f487908cb121c0
ITSC <sca@koreaedugroup.com>
2023-09-09 15:05:33 +0900

    Ex12

---
+++ section12/Ex12-1-module.py
@@ -0,0 +1,29 @@
+'''
+파일명: Ex12-1-module.py
+
+모듈(module)
+    변수나 함수 또는 클래스를 모아 놓은 파일을 모듈이라고 한다.
+    모듈 => 파일명.py
+
+모듈 사용법 
+import 모듈명
+
+'''
+
+import converter
+
+miles = converter.kilometer_to_miles(150)
+print(f'150km = {miles}miles')
+
+pounds = converter.gram_to_pounds(1000)
+print(f'1000g = {pounds}pounds')
+
+
+
+
+
+
+
+
+
+
---
+++ section12/Ex12-2-module2.py
@@ -0,0 +1,8 @@
+'''
+파일명: Ex12-2-module2.py
+
+모듈 사용법
+from 모듈명 import 함수
+from 모듈명 import 함수1, 함수2
+from 모듈명 import *
+'''
