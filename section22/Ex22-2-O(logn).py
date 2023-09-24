e8d3dae4b8d07786e25c92b569c70cc263946bdc
ITSC <sca@koreaedugroup.com>
2023-09-24 17:01:04 +0900

    ex22 빅오표기법

--- section22/Ex22-2-O(logn).py
+++ section22/Ex22-2-O(logn).py
@@ -11,7 +11,7 @@ def binary_search(arr, x):
     high = len(arr) - 1
 
     while low <= high:
-        mid = (low + high)  // 2
+        mid = (low + high) // 2
 
         if arr[mid] < x:
             low = mid + 1
