e87e3c2841c3ff6bad9f24d5e788fecb22ee6d97
ITSC <sca@koreaedugroup.com>
2023-09-24 17:20:08 +0900

    ex22 빅오표기법

--- section22/Ex22-2-O(logn).py
+++ section22/Ex22-2-O(logn).py
@@ -4,13 +4,28 @@
 O(logN)
     로그 시간 복잡도, 이진 탐색처럼 문제를 절반으로 나누어 해결하는 알고리즘
 '''
+
+
 def binary_search(arr, x):
+
     # 검색 범위 시작점 설정
     low = 0
     # 검색 범위 끝점 설정
     high = len(arr) - 1
 
+    '''
+    arr = [1, 3, 5, 7, 8, 9, 10, 11, 21]
+    
+    low = 8
+    high = 8
+    mid = 8
+    arr[mid] = 21
+    x = 21
+    '''
+    num = 0
     while low <= high:
+        num += 1
+        print(num)
         mid = (low + high) // 2
 
         if arr[mid] < x:
---
+++ section22/Ex22-3-O(N).py
@@ -0,0 +1,21 @@
+'''
+파일명: Ex22-3-O(N).py
+
+O(N)
+    선형 시간 복잡도, 입력 크기에 비례하여
+    시간이 걸리는 알고리즘
+'''
+
+def linear_search(arr, x):
+    size = len(arr)
+    for i in range(size):
+        if arr[i] == x:
+            return i
+
+    return -1
+
+# 실행코드
+arr = [1, 3, 5, 7, 9]
+print(linear_search(arr, 9))
+
+
