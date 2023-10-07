e102a80b8f2d09b2f39ff585d6cf7c7999b8a2ac
ITSC <sca@koreaedugroup.com>
2023-10-07 17:00:28 +0900

    ex23 정렬

--- section23/Ex23-4-QuickSort.py
+++ section23/Ex23-4-QuickSort.py
@@ -7,10 +7,60 @@
     각 부분 리스트에 대해 재귀적으로 퀵정렬을 수행하는 알고리즘
 
 '''
+
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr
 
+    ''' [1, 2, 3, 4, 5, 6, 7, 8]
+    
+           0  1  2  3  4  5  6  7
+    arr = [6, 5, 3, 1, 8, 7, 2, 4]
+    pivot = 6
+    left  = [5, 3, 1, 2, 4]
+    right = [8, 7]
+    equal = [6]
+    a = 6 
+    a = 5
+    
+    quick_sort(left)                    => [1, 2, 3, 4, 5]
+        arr = [5, 3, 1, 2, 4]
+        pivot = 5
+        left  = [3, 1, 2, 4]
+        right = []
+        equal = [5]
+        quick_sort(left)         => [1, 2, 3, 4]
+            arr  = [3, 1, 2, 4]
+            pivot = 3
+            left  = [1, 2]
+            right = [4]
+            equal = [3]
+            quick_sort(left)    => [1, 2]
+                arr = [1, 2]
+                pivot = 1
+                left  = []
+                right = [2]
+                equal = [1]
+                [] + [1] + [2]     
+                
+            equal               => [3]
+            quick_sort(right)   => [4]
+        equal                        =>   [5]     
+        quick_sort(right)            =>   []
+        
+    equal                                      => [6]
+    
+    quick_sort(right)   => [7, 8]
+        arr  = [8, 7]
+        pivot = 8
+        left  = [7]
+        right = []
+        equal = [8]
+        
+    [1, 2, 3, 4, 5] + [6] + [7, 8]
+        
+    
+    '''
     pivot = arr[0]
 
     left, right, equal = [], [], []
